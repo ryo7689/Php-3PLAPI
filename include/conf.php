255acf19d6109b9af1e8397a128ef59e6e908d3c
ryo7689 <ryo7689@hotmail.com>
2017-05-30 09:36:25 +1200

    Create conf.php

---
+++ include/conf.php
@@ -0,0 +1,11 @@
+<?php
+
+$threePlId  = ; #threePLId
+$threePlKey = ; #threePLKey
+$username   = ; #Client area login name
+$pass       = ; #Client area password
+$facilityId = ; #Facility Id
+$customerId = ; #Customer Id
+
+
+?>
